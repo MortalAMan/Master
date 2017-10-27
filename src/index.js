1c0796f1ce259cfeb89df7a52ae306df77d26f5c
Addy <Ashepherdking@gmail.com>
2017-10-27 01:09:39 -0600

    slight fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './css/App.css';
+import './css/Index.css';
 import App from './components/App';
-import Header from './components/Header';
-import Body from './components/Body';
+
 
 render(<App/>, document.querySelector('#main'));
